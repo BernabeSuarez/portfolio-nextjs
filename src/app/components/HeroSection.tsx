d602e8552456294228e1451437bb9258285736c8
Bernabe Suarez <bernatattoo@gmail.com>
2024-02-05 12:49:11 -0300

    tadd yping effect to hero

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,34 +1,55 @@
+"use client";
+
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 export default function Hero() {
   // eslint-disable-next-line react/no-unescaped-entities
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12 gap-12">
-        <div className="col-span-7 place-self-center">
+        <div className=" col-span-7 place-self-center">
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold text-center lg:text-left">
-            Hello I`m Bernabé
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to bg-pink-500">
+              Hello I`m{" "}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Bernabé",
+                1500, // wait 1s before replacing "Mice" with "Hamsters"
+                "Web Developer",
+                1500,
+                "Python Developer",
+                1500,
+                "FullStack Developer",
+                1500,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl text-center lg:text-left">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
             laborum esse, repellat quo molestiae vero. Vel ullam vero deleniti
             beatae, laborum quia dicta sit ipsa quidem in, corrupti, doloremque
             id.
           </p>
           <div>
-            <button className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 border bg-white text-black hover:bg-slate-200 mb-6 mt-6">
+            <button className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4  bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white mb-6 mt-6">
               Contactarme
             </button>
-            <button
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 
-            border  bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white "
-            >
-              Descargar CV
+            <button className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Descargar CV
+              </span>
             </button>
           </div>
         </div>
-        <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative">
-          <div className="rounded-full">
+        <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative sm:ml-10">
+          <div className="flex rounded-full justify-items-center">
             <Image
               src="/images/hero-image.jpg"
               alt="Hero Image"
