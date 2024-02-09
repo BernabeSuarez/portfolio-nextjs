7686daf53067c7430a1945c04dcbee5846d6a4f7
Bernabe Suarez <bernatattoo@gmail.com>
2024-02-09 01:42:46 -0300

    Add components project section

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,27 +1,36 @@
 import Image from "next/image";
 
-const ProjectCard = () => {
-  return (
-    <div className="group relative m-0 flex h-72 w-96 rounded-xl shadow-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg">
-      <div className="z-10 h-full w-full overflow-hidden rounded-xl border border-gray-200 opacity-80 transition duration-300 ease-in-out group-hover:opacity-100 dark:border-gray-700 dark:opacity-70">
-        <Image
-          src="/images/about-section-img.jpg"
-          className="animate-fade-in block h-full w-full scale-100 transform object-cover object-center opacity-100 transition duration-300 group-hover:scale-110"
-          alt=""
-          width={300}
-          height={300}
-        />
-      </div>
-      <div className="absolute bottom-0 z-20 m-0 pb-4 ps-4 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110">
-        <h1 className="font-serif text-2xl font-bold text-white shadow-xl">
-          Azores
-        </h1>
+interface ICardData {
+  imgUrl: string;
+  title: string;
+  description: string;
+  href: string;
+}
 
-        <h1 className="text-sm font-light text-gray-200 shadow-xl">
-          A Little Paradise in Portugal
-        </h1>
+const ProjectCard = (carData: ICardData) => {
+  return (
+    <a href={carData.href} target="_blank">
+      <div className="group relative m-0 flex h-72 w-96 rounded-xl shadow-xl ring-gray-900/5 sm:mx-auto sm:max-w-lg">
+        <div className="z-10 h-full w-full overflow-hidden rounded-xl border border-gray-200 opacity-80 transition duration-300 ease-in-out group-hover:opacity-100 dark:border-gray-700 dark:opacity-70">
+          <Image
+            src={carData.imgUrl}
+            className="animate-fade-in block h-full w-full scale-100 transform object-cover object-center opacity-100 transition duration-300 group-hover:scale-110"
+            alt=""
+            width={300}
+            height={300}
+          />
+        </div>
+        <div className="absolute bottom-0 z-20 m-0 pb-4 ps-4 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110">
+          <h2 className="font-serif text-2xl font-bold text-white shadow-xl">
+            {carData.title}
+          </h2>
+
+          <p className="text-sm font-light text-gray-200 shadow-xl">
+            {carData.description}
+          </p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
--- src/app/components/ProjectSection.tsx
+++ src/app/components/ProjectSection.tsx
@@ -1,18 +1,54 @@
 import ProjectCard from "./ProjectCard";
 
+const ProjectsData = [
+  {
+    id: 1,
+    title: "Proyecto n1",
+    description: "PAGINA WEB DE GRUPO MUSICAL",
+    imgUrl: "/images/about-section-img.jpg",
+    href: "https://laotrajunta.com.ar/",
+  },
+  {
+    id: 2,
+    title: "Proyecto n2",
+    description: "PAGINA WEB DE GRUPO MUSICAL",
+    imgUrl: "/images/about-section-img.jpg",
+    href: "",
+  },
+  {
+    id: 4,
+    title: "Proyecto n4",
+    description: "PAGINA WEB DE GRUPO MUSICAL",
+    imgUrl: "/images/about-section-img.jpg",
+    href: "",
+  },
+  {
+    id: 1,
+    title: "Proyecto n4",
+    description: "PAGINA WEB DE GRUPO MUSICAL",
+    imgUrl: "/images/about-section-img.jpg",
+    href: "",
+  },
+];
+
+//Agregarun filtro por el tipo de proyecto
+
 const ProjectSection = () => {
   return (
     <section className="flex flex-col justify-center" id="projects">
       <h2 className="text-center text-4xl font-semibold mb-4 text-white">
         My Projects
       </h2>
-      <div className="grid grid-cols-3 grid-rows-3 gap-8">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+      <div className="grid grid-cols-1 md:grid-cols-3 grid-rows-3 gap-8">
+        {ProjectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            imgUrl={project.imgUrl}
+            description={project.description}
+            href={project.href}
+          />
+        ))}
       </div>
     </section>
   );
