486396e96e57673ecc38ab0278376ab69bec63c7
Bernabe Suarez <bernatattoo@gmail.com>
2024-02-11 01:24:50 -0300

    Add hover navbar

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -9,7 +9,7 @@ const NavLink = (linkData: LinkData) => {
   return (
     <Link
       href={linkData.href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+      className="block py-2 pl-3 pr-4 text-[#ADB7BE] text-2xl sm:text-xl  md:p-0 hover:text-[#e45400] hover:transition-all  duration-200"
     >
       {linkData.title}
     </Link>
